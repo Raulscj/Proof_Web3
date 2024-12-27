5575132d0e8b4df1bf2f5b4df912f1c12cbf4c7c
Raulscj <radscjggdiosteama@gmail.com>
2024-12-27 13:27:35 -0400

    auth and signature to login ready

--- additional-env.d.ts
+++ additional-env.d.ts
@@ -5,6 +5,7 @@ declare global {
       NEXT_PUBLIC_W3C_PID: string;
       NEXT_PUBLIC_SIGNIN_MESSAGE: string;
       NEXTAUTH_SECRET: string;
+
       SIWE_DOMAIN: string;
       NEXT_PUBLIC_GREETING_CONTRACT: `0x${string}`;
       NEXT_PUBLIC_NFT_CONTRACT: `0x${string}`;
